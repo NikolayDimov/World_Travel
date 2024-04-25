346f0cfeca3423310bf80d6e07acc556414175bb
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-04-26 00:01:07 +0300

    feat(App): add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Product from "./pages/Product/Product";
+import Homepage from "./pages/Homepage/Homepage";
+import Pricing from "./pages/Pricing/Pricing";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+
 function App() {
-    return <>World Travel</>;
+    return (
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
+    );
 }
 
 export default App;
