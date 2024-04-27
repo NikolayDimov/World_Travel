deb364b2967ae0969a37ebcbcda087a7598356c3
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-04-27 23:59:00 +0300

    feat(Map): add geolocation button, marker popup, ChangeCenter, DetectClick

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
 import { useState } from "react";
 import { useCities } from "../../contexts/CitiesContext";
+import { useGeolocation } from "../../hooks/useGeolocation";
+import Button from "../Button/Button";
 
 function Map() {
     const { cities } = useCities();
     const [searchParams] = useSearchParams();
+    const { isLoading: isLoadingPosition, position: goelocationPostion, getPosition } = useGeolocation();
 
     const defaultLat: number = 40;
     const defaultLng: number = 0;
@@ -28,15 +31,26 @@ function Map() {
         }
     }, [mapLat, mapLng]);
 
+    useEffect(() => {
+        if (goelocationPostion) {
+            setMapPosition([goelocationPostion.lat, goelocationPostion.lng]);
+        }
+    }, [goelocationPostion]);
+
     return (
         <div className={styles.mapContainer}>
+            {!goelocationPostion && (
+                <Button type="position" onClick={getPosition}>
+                    {isLoadingPosition ? "Loading..." : "Use your position"}
+                </Button>
+            )}
             <MapContainer className={styles.map} center={mapPosition} zoom={8} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
                 {cities.map((city) => (
-                    <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                    <Marker position={[parseFloat(city.position.lat), parseFloat(city.position.lng)]} key={city.id}>
                         <Popup>
                             <span>{city.emoji}</span> <span>{city.cityName}</span>
                         </Popup>
@@ -44,6 +58,7 @@ function Map() {
                 ))}
 
                 <ChangeCenter position={mapPosition as [number, number]} />
+                <DetectClick />
             </MapContainer>
         </div>
     );
@@ -61,6 +76,24 @@ function ChangeCenter({ position }: ChangeCenterProps) {
         map.setView(position, map.getZoom());
     }
 
+    return null;
+}
+
+interface CustomMouseEvent {
+    latlng: {
+        lat: number;
+        lng: number;
+    };
+}
+
+export function DetectClick() {
+    const navigate = useNavigate();
+    useMapEvents({
+        click: (e: CustomMouseEvent) => {
+            console.log(e);
+            navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+        },
+    });
     return null;
 }
 
